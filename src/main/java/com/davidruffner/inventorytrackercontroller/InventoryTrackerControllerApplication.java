82edc65f9214c94fb7e69101bd6fddbb9b23e260
David Ruffner <davidruffner924@gmail.com>
2024-03-03 09:47:38 -0500

    Initial commit

---
+++ src/main/java/com/davidruffner/inventorytrackercontroller/InventoryTrackerControllerApplication.java
@@ -0,0 +1,13 @@
+package com.davidruffner.inventorytrackercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryTrackerControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryTrackerControllerApplication.class, args);
+    }
+
+}
